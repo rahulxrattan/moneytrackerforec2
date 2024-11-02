64de815868b381261338e5121074f87d87edf470
Rahul <112358785+rahulxrattan@users.noreply.github.com>
2024-11-02 13:29:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    // port:3000,
+    host: '0.0.0.0',
     proxy:{
       '/api':{
         target: 'http://34.197.94.125:5000',
